f826ec755168c66f77e39b122f60f32c3ec97df4
nikitagagua <2gagua121@gmail.com>
2024-05-02 22:31:10 +0300

    Initial commit

---
+++ src/main/java/com/testtask/ClearSolution/ClearSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.testtask.ClearSolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearSolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testtask/ClearSolution/ClearSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testtask.ClearSolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
