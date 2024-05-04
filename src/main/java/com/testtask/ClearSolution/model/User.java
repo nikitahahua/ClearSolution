c1712a6696dc8a28bd50528b108c8a18f6971444
nikitagagua <2gagua121@gmail.com>
2024-05-05 00:35:29 +0300

    dev: add user model

---
+++ src/main/java/com/testtask/ClearSolution/model/User.java
@@ -0,0 +1,52 @@
+package com.testtask.ClearSolution.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Pattern;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Getter @Setter
+@Table(name = "users")
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Pattern(regexp = "[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}", message = "Must be a valid e-mail address")
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @JsonFormat(pattern="yyyy-MM-dd")
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    public User(String email, String firstName, String lastName, LocalDate birthDate, String address, String phoneNumber) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public User(String email, String firstName, String lastName, LocalDate birthDate) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
+}
